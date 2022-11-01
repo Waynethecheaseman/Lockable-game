518b7182a75a52ef4cca074c10ca0eeabb630b85
Waynethecheaseman <89491712+Waynethecheaseman@users.noreply.github.com>
2022-10-31 21:39:06 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,63 @@
+input.onButtonPressed(Button.A, function () {
+    if (Pins == 0) {
+        Pins += 1
+    }
+    if (Pins == 3) {
+        sprite.change(LedSpriteProperty.Y, -2)
+        basic.pause(500)
+        sprite.change(LedSpriteProperty.Y, 2)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Pins == 2) {
+        Pins += 1
+        if (Pins == 3) {
+        	
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (Pins == 1) {
+        Pins += 1
+    }
+    if (Pins == 3) {
+    	
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (Pins == 3) {
+        sprite = game.createSprite(0, 4)
+        sprite1 = game.createSprite(4, 4)
+        gamestart += 1
+    }
+})
+let points = 0
+let gamestart = 0
+let sprite1: game.LedSprite = null
+let sprite: game.LedSprite = null
+let Pins = 0
+Pins = 0
+basic.forever(function () {
+    if (gamestart == 1) {
+        if (sprite.isTouching(sprite1)) {
+            basic.showString("" + (points))
+            gamestart = 3
+        }
+    }
+})
+basic.forever(function () {
+    if (gamestart == 1) {
+        for (let index = 0; index < 5; index++) {
+            basic.pause(200)
+            sprite1.change(LedSpriteProperty.X, -1)
+        }
+        points += 1
+        sprite1.delete()
+        sprite1 = game.createSprite(4, 4)
+    }
+})
+basic.forever(function () {
+    if (gamestart == 3) {
+        game.gameOver()
+    }
+})
